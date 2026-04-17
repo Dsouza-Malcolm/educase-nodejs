599f26996851d9cd407ce96fab09cd5819205d73
Dsouza-Malcolm <malcolm.dsouza.dev@gmail.com>
2026-04-17 16:44:07 +0530

    fixed env configuration bug

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,13 +1,14 @@
 import z from "zod";
 import dotenv from "dotenv";
 
-dotenv.config({ path: ".env.local" });
+dotenv.config();
 
 const EnvSchema = z.object({
   DB_HOST: z.string().nonempty(),
   DB_PASSWORD: z.string().nonempty(),
   DB_USER: z.string().nonempty(),
   DB_NAME: z.string().nonempty(),
+  DB_PORT: z.coerce.number().default(3306),
   PORT: z.coerce.number(),
 });
 
